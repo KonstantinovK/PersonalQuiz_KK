8b0a6a6451db84ad4b9e2de8795a6311a7fabced
KonstantinKonstantinov <konstantinov87@mail.ru>
2022-03-16 10:55:52 +0300

    Initial Commit

---
+++ PersonalQuiz_KK/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PersonalQuiz_KK
+//
+//  Created by MacBook on 16.03.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
